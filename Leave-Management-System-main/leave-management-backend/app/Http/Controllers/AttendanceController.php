b8af4d972dd8eaea4065788985567a3e739fc85f
Zakaria123-hs <zakariahasbi19@gmail.com>
2026-07-16 18:26:25 +0100

    update generateTokn function to optimize storage in data base now we have just one row we don't need the other token that expired

--- Leave-Management-System-main/leave-management-backend/app/Http/Controllers/AttendanceController.php
+++ Leave-Management-System-main/leave-management-backend/app/Http/Controllers/AttendanceController.php
@@ -7,22 +7,21 @@ use Illuminate\Http\Request;
 class AttendanceController extends Controller
 {
     public function generateToken()
-    {
-        // Deactivate old tokens to keep the database clean
-        QrCode::where('is_active', true)->update(['is_active' => false]);
+        {
+            QrCode::query()->delete();
 
-        // Create a new token that expires in 15 seconds
-        $qrCode = QrCode::create([
-            'token' => Str::random(64),
-            'expires_at' => Carbon::now()->addSeconds(15),
-            'is_active' => true,
-        ]);
+            // 2. Create the single, active token
+            $qrCode = QrCode::create([
+                'token' => Str::random(64),
+                'expires_at' => Carbon::now()->addSeconds(15),
+                'is_active' => true,
+            ]);
 
-        return response()->json([
-            'token' => $qrCode->token,
-            'expires_in_seconds' => 15
-        ], 200);
-    }
+            return response()->json([
+                'token' => $qrCode->token,
+                'expires_in_seconds' => 15
+            ], 200);
+        }
     public function scanToken(Request $request)
     {
         $request->validate([
